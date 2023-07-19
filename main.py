c4b6a228ab5e7094f2f6a6fabc14d7e9a8d4dcfa
Kaloian Kozlev <kbkozlev@gmail.com>
2023-07-19 17:31:31 +0300

    Basic outlines on GUI

---
+++ main.py
@@ -0,0 +1,64 @@
+import PySimpleGUI as sg
+from psgtray import SystemTray
+
+
+def main_window():
+    tray_menu = ['', ['Show Window', 'Hide Window', '---', 'Exit']]
+    app_menu = [['Help', ['About', 'Check for Updates']]]
+
+    layout = [[sg.Menubar(app_menu)],
+              [sg.T('Delete Frequency:'), sg.T('M:'),
+               sg.DropDown(months, default_value='0', key='-M-'), sg.T('D:'),
+               sg.DropDown(days, default_value='0', key='-D-'), sg.T('H:'),
+               sg.DropDown(hours, default_value='0', key='-H-'), sg.T('M:'),
+               sg.DropDown(minutes, default_value='0', key='-M-')],
+              [sg.HSeparator()],
+              [sg.Checkbox('Start on boot', key='-ONBOOT-')],
+              [sg.Button('Apply'), sg.Button('Exit')]
+              ]
+
+    window = sg.Window(window_title, layout, icon=icon, font=(font_family, font_size), finalize=True,
+                       enable_close_attempted_event=True)
+    tray = SystemTray(tray_menu, single_click_events=False, window=window, tooltip='BinCleaner', icon=icon)
+
+    while True:
+        event, values = window.read()
+
+        if event == tray.key:
+            event = values[event]
+
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+
+        if event in ('Show Window', sg.EVENT_SYSTEM_TRAY_ICON_DOUBLE_CLICKED):
+            window.un_hide()
+            window.bring_to_front()
+        elif event in ('Hide Window', sg.WIN_CLOSE_ATTEMPTED_EVENT):
+            window.hide()
+            tray.show_message('BinCleaner', 'BinCleaner minimized to tray!')
+            tray.show_icon()
+
+        if event == 'Apply':
+            print(values['-M-'])
+
+    tray.close()
+    window.close()
+
+if __name__ == "__main__":
+    release = '1.0.0'
+    window_title = f"BinCleaner v{release}"
+    font_family = "Arial"
+    font_size = 10
+    bt_color = "#015FB8"
+    icon = "BinCleaner.ico"
+
+    sg.theme("Reddit")
+    sg.set_options(font=(font_family, font_size), force_modal_windows=True, dpi_awareness=True,
+                   auto_size_buttons=True, auto_size_text=True)
+
+    months = list(range(0, 13))
+    days = list(range(0, 31))
+    hours = list(range(0, 24))
+    minutes = list(range(0, 60))
+
+    main_window()
