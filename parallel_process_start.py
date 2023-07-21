b07599e8a52c4d5ed6b140024de598f7c958a781
Kaloian Kozlev <kbkozlev@gmail.com>
2023-07-21 11:22:54 +0300

    removed file

--- parallel_process_start.py
+++
@@ -1,8 +0,0 @@
-from concurrent.futures import ProcessPoolExecutor
-
-
-def run_tasks_in_parallel(tasks):
-    with ProcessPoolExecutor() as executor:
-        running_tasks = [executor.submit(task) for task in tasks]
-        for running_task in running_tasks:
-            running_task.result()
